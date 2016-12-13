216f0a3616305e95c0e6a1908a87ad17d2234383
Harmen <harm@dds.nl>
2016-12-13 16:33:56 +0100

    update runit

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.4"
+version          "3.1.0"
 
 %w{ unicorn apache2 passenger_apache2 nginx }.each do |cb|
   depends cb
 end
 
 depends "application", "~> 4.0"
-depends "runit", "~> 1.0"
+depends "runit", "~> 3.0"
 depends "logrotate", "~> 1.0"
 depends "apt"
 depends "nginx"
